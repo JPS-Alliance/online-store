2035aae11d28b87140f5eda413b3b68839f5a99d
Johan Sebastiampillai <johan15_seb@hotmail.com>
2025-12-15 13:54:00 -0500

    Updates

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    domains: ["picsum.photos"], // add any external domains here
-  },
 };
 
 export default nextConfig;
